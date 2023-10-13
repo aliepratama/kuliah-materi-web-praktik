b8fe6d73e8d581ad4033b0893be3efea18c2b965
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2023-10-13 15:24:08 +0700

    konfigurasi mysql dengan flask

--- myapp.py
+++ myapp.py
@@ -1,9 +1,17 @@
 # import flask
 from flask import Flask, render_template
-
+from flask_mysqldb import MySQL
 #main app
 app = Flask(__name__)
 
+#database config
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'latihan_flask'
+#init mysql
+mysql = MySQL(app)
 # set route untuk /
 @app.route('/')
 #function index
@@ -15,6 +23,13 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/users/')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('users.html', users=data)
 #debug, untuk update server dev otomatis
 if __name__ == "__main__":
     app.run(debug=True)
