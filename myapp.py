ff836075d88f9514bf9e845d4ff0832956c434c9
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2023-10-20 14:25:33 +0700

    feat: konfigurasi session

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 # import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_mysqldb import MySQL
 #main app
 app = Flask(__name__)
@@ -13,14 +13,42 @@ app.config['MYSQL_DB'] = 'latihan_flask'
 #init mysql
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users")
-    data = cur.fetchall()
-    cur.close()
-    return render_template('index.html', users=data)
+# @app.route('/')
+# def index():
+#     cur = mysql.connection.cursor()
+#     cur.execute("SELECT * FROM users")
+#     data = cur.fetchall()
+#     cur.close()
+#     return render_template('index.html', users=data)
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'inpEmail' in request.form and 'inpPass' in request.form:
+        email = request.form['inpEmail']
+        passwd = request.form['inpPass']
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, passwd))
+        result = cur.fetchone()
+        if result:
+            session['is_logged_in'] = True
+            session['username'] = result[1]
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+@app.route('/home')
+def home():
+    if 'is_logged_in' in session:
+        return render_template('home.html')
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('is_logged_in', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 @app.route('/about/')
 def about():
     return render_template('about.html')
